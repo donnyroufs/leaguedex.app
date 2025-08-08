dcca4dd806e7cf2acc98447cc8e178bf6f0085b4
donny <iamchets@yahoo.com>
2025-08-08 13:20:59 +0200

    fix routing error on prod
    guessing here

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -7,12 +7,25 @@ import { createBrowserRouter, RouterProvider } from 'react-router'
 import { Matchups } from './pages/Matchups'
 import { Layout } from './Layout'
 import { Settings } from './pages/Settings'
+import { Titlebar } from './components/Titlebar'
 
 const router = createBrowserRouter([
   {
     element: <Layout />,
+    errorElement: (
+      <div className="flex flex-col h-screen w-screen">
+        <Titlebar title="Leaguedex" />
+        <div className="flex-1 flex flex-col items-center justify-center">
+          <h1 className="text-2xl font-bold">Something went wrong</h1>
+          <p className="mt-4">
+            Please try restarting the app. If the problem persists, please contact support.
+          </p>
+        </div>
+      </div>
+    ),
     children: [
       {
+        index: true,
         path: '/',
         element: <Matchups />
       },
