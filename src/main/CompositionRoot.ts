dd3d90583172b3584259a827e198db24c0906a9a
donny <iamchets@yahoo.com>
2025-08-26 12:04:10 +0200

    make imports clear

--- src/main/CompositionRoot.ts
+++ src/main/CompositionRoot.ts
@@ -1,49 +1,23 @@
 import path from 'path'
+import axios from 'axios'
 import { app, type IpcMain } from 'electron'
 
-import {
-  FileSystemReminderRepository,
-  FakeReminderRepository,
-  NativeWindowsSpeechGenerator,
-  OpenAISpeechGenerator,
-  DevSpeechGenerator
-} from './adapters/outbound'
-import { GameObjectiveTracker, IReminderRepository, RemoveReminderUseCase } from './hexagon'
+import * as Hexagon from './hexagon'
+import * as Outbound from './adapters/outbound'
+import * as Inbound from './adapters/inbound'
+
 import { App } from './Leaguedex'
-import { ElectronAdapter } from './adapters/inbound'
-import { EventBus } from './adapters/outbound'
-import { GameDetectionService } from './hexagon'
-import { IEventBus } from './hexagon'
-import { INotifyElectron } from './hexagon'
-import { NotifyElectron } from './adapters/outbound'
-import { Timer } from './adapters/outbound'
-import { ILogger } from './hexagon'
-import { IAudioPlayer } from './hexagon'
-import { AudioPlayer } from './adapters/outbound'
-import { ElectronLogger } from './adapters/outbound'
-import {
-  IRiotClientDataSource,
-  RiotClientDataSource,
-  SimulatedRiotClientDataSource,
-  RiotLiveClientApi
-} from './adapters/outbound/game-data'
-import { ITextToSpeechGenerator } from './hexagon'
-import { ITimer } from './hexagon'
-import { CreateReminderUseCase } from './hexagon'
-import { GetRemindersUseCase } from './hexagon'
-import { RemindersGameTickListener } from './hexagon'
-import axios from 'axios'
 import { getLicenseKey } from './getLicenseKey'
 
 type AppDependencies = {
-  eventBus: IEventBus
-  dataSource: IRiotClientDataSource
-  notifyElectron: INotifyElectron
-  timer: ITimer
-  reminderRepository: IReminderRepository
-  logger: ILogger
-  audioPlayer: IAudioPlayer
-  tts: ITextToSpeechGenerator
+  eventBus: Hexagon.IEventBus
+  dataSource: Outbound.IRiotClientDataSource
+  notifyElectron: Hexagon.INotifyElectron
+  timer: Hexagon.ITimer
+  reminderRepository: Hexagon.IReminderRepository
+  logger: Hexagon.ILogger
+  audioPlayer: Hexagon.IAudioPlayer
+  tts: Hexagon.ITextToSpeechGenerator
   endTimer: number
 }
 
@@ -59,55 +33,61 @@ export async function createApp(
 
   // Cross cutting concerns
   const dataPath = isPackaged ? app.getPath('userData') : path.join(process.cwd(), 'data')
-  const logger = overrides.logger ?? new ElectronLogger(path.join(dataPath, 'logs.log'), 'info')
-  const eventBus = overrides.eventBus ?? new EventBus(logger)
+  const logger =
+    overrides.logger ?? new Outbound.ElectronLogger(path.join(dataPath, 'logs.log'), 'info')
+  const eventBus = overrides.eventBus ?? new Outbound.EventBus(logger)
   const dataSource =
     overrides.dataSource ??
     (isProd
-      ? new RiotClientDataSource()
-      : SimulatedRiotClientDataSource.createAndStartGame(overrides.endTimer ?? 600))
-  const gameDataProvider = new RiotLiveClientApi(dataSource)
-  const notifyElectron = overrides.notifyElectron ?? new NotifyElectron()
+      ? new Outbound.RiotClientDataSource()
+      : Outbound.SimulatedRiotClientDataSource.createAndStartGame(overrides.endTimer ?? 600))
+  const gameDataProvider = new Outbound.RiotLiveClientApi(dataSource)
+  const notifyElectron = overrides.notifyElectron ?? new Outbound.NotifyElectron()
 
-  const timer = overrides.timer ?? new Timer()
-  const gameDetectionService = new GameDetectionService(eventBus, gameDataProvider, timer, logger)
+  const timer = overrides.timer ?? new Outbound.Timer()
+  const gameDetectionService = new Hexagon.GameDetectionService(
+    eventBus,
+    gameDataProvider,
+    timer,
+    logger
+  )
 
   // Modules
-  let reminderRepository: IReminderRepository
+  let reminderRepository: Hexagon.IReminderRepository
 
   if (isProd && overrides.reminderRepository == null) {
-    reminderRepository = await FileSystemReminderRepository.create(dataPath)
+    reminderRepository = await Outbound.FileSystemReminderRepository.create(dataPath)
   } else {
-    reminderRepository = overrides.reminderRepository ?? new FakeReminderRepository()
+    reminderRepository = overrides.reminderRepository ?? new Outbound.FakeReminderRepository()
   }
 
-  const audioPlayer = overrides.audioPlayer ?? new AudioPlayer(logger, isProd)
+  const audioPlayer = overrides.audioPlayer ?? new Outbound.AudioPlayer(logger, isProd)
   const audioDir = path.join(dataPath, 'audio')
-  let tts: ITextToSpeechGenerator
+  let tts: Hexagon.ITextToSpeechGenerator
 
   const licenseKey = await getLicenseKey()
 
   // TODO: if we enter a license key we might have to update, or restart the app.
   if (isProd && licenseKey.length > 0) {
-    tts = OpenAISpeechGenerator.create(axiosInstance, audioDir)
+    tts = Outbound.OpenAISpeechGenerator.create(axiosInstance, audioDir)
   } else if (isProd && !licenseKey) {
-    tts = await NativeWindowsSpeechGenerator.create(logger, audioDir)
+    tts = await Outbound.NativeWindowsSpeechGenerator.create(logger, audioDir)
   } else {
-    tts = new DevSpeechGenerator()
+    tts = new Outbound.DevSpeechGenerator()
   }
 
   tts = overrides.tts ?? tts
 
-  const gameObjectiveTracker = new GameObjectiveTracker(logger)
-  const remindersGameTickListener = new RemindersGameTickListener(
+  const gameObjectiveTracker = new Hexagon.GameObjectiveTracker(logger)
+  const remindersGameTickListener = new Hexagon.RemindersGameTickListener(
     reminderRepository,
     audioPlayer,
     logger,
     gameObjectiveTracker
   )
-  const createReminderUseCase = new CreateReminderUseCase(tts, reminderRepository)
-  const getRemindersUseCase = new GetRemindersUseCase(reminderRepository)
-  const removeReminderUseCase = new RemoveReminderUseCase(reminderRepository)
+  const createReminderUseCase = new Hexagon.CreateReminderUseCase(tts, reminderRepository)
+  const getRemindersUseCase = new Hexagon.GetRemindersUseCase(reminderRepository)
+  const removeReminderUseCase = new Hexagon.RemoveReminderUseCase(reminderRepository)
 
   if (isPackaged) {
     const os = await import('os')
@@ -137,11 +117,11 @@ export async function createApp(
 
 export async function createTestApp(overrides: Partial<AppDependencies> = {}): Promise<App> {
   if (overrides?.logger == null) {
-    overrides.logger = ElectronLogger.createNull()
+    overrides.logger = Outbound.ElectronLogger.createNull()
   }
 
   if (overrides.tts == null) {
-    overrides.tts = new DevSpeechGenerator()
+    overrides.tts = new Outbound.DevSpeechGenerator()
   }
 
   return createApp(overrides, false)
@@ -151,7 +131,7 @@ export async function createAppAndStart(ipcMain?: IpcMain): Promise<void> {
   const leaguedex = await createApp({}, app.isPackaged)
 
   if (ipcMain) {
-    await ElectronAdapter.setup(leaguedex, ipcMain)
+    await Inbound.ElectronAdapter.setup(leaguedex, ipcMain)
   }
 
   await leaguedex.start()
