da94ebbc59fc1e92678831b1cfb5c51365f3ba1f
Donny Roufs <iamchets@yahoo.com>
2025-11-06 22:10:13 +0100

    fix linting issues

--- tests/LoggerSpy.ts
+++ tests/LoggerSpy.ts
@@ -1,7 +1,16 @@
 import { ILogger } from '../src/main/hexagon/ports/ILogger'
 
 export class LoggerStub implements ILogger {
-  public info(): void {}
-  public error(): void {}
-  public debug(): void {}
+  public info(): void {
+    return
+  }
+  public error(): void {
+    return
+  }
+  public debug(): void {
+    return
+  }
+  public warn(): void {
+    return
+  }
 }
