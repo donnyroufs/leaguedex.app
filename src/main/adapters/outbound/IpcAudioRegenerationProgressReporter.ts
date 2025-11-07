b7108ff6a9f23211c37f4b274ccd168195174bfc
Donny Roufs <iamchets@yahoo.com>
2025-11-07 12:14:05 +0100

    fix progress modal being stuck when window not focused

--- src/main/adapters/outbound/IpcAudioRegenerationProgressReporter.ts
+++ src/main/adapters/outbound/IpcAudioRegenerationProgressReporter.ts
@@ -8,15 +8,19 @@ export class IpcAudioRegenerationProgressReporter implements IAudioRegenerationP
     completedCues: number,
     totalUniqueCues: number
   ): void {
-    BrowserWindow.getFocusedWindow()?.webContents.send('regenerate-audio-progress', {
-      completedPacks,
-      totalPacks,
-      completedCues,
-      totalUniqueCues
+    BrowserWindow.getAllWindows().forEach((window) => {
+      window.webContents.send('regenerate-audio-progress', {
+        completedPacks,
+        totalPacks,
+        completedCues,
+        totalUniqueCues
+      })
     })
   }
 
   public reportComplete(): void {
-    BrowserWindow.getFocusedWindow()?.webContents.send('regenerate-audio-complete')
+    BrowserWindow.getAllWindows().forEach((window) => {
+      window.webContents.send('regenerate-audio-complete')
+    })
   }
 }
